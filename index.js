10b1cf78f0aa6150c8ed6cb74ad33cc477441771
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-08-11 15:15:49 -0400

    commit tree d4bb1de762ef3e515cbac9f53e2a12c4d452e56a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(function (driverName){
+        return driverName.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter((driverName) => driverName[0].toLowerCase() === string[0].toLowerCase())
+}
+
+function matchName(drivers, string){
+    return drivers.filter((driver) => driver.name.toLowerCase() === string.toLowerCase())
+}
